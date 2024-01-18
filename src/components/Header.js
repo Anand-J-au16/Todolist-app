81098ad42a5a09343fd989592fde1dace49201bb
J Anand <59288148+Anand-J-au16@users.noreply.github.com>
2024-01-18 19:12:51 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
+
+function Header() {
+    return (
+    <header>
+        <h1>
+            <LocalFloristIcon />
+            ToDoList
+        </h1>
+   </header>
+    );
+}
+
+export default Header;
